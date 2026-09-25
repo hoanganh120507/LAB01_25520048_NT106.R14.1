ea07034631a0a58ff8f7ce5c1d4123475673691a
Anh <hoanganh12052007@gmail.com>
2026-09-25 14:31:14 +0700

    updated bai 04

--- lab01/Lab01_Bai04.Designer.cs
+++ lab01/Lab01_Bai04.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Lab01_Bai04";
+            label1 = new Label();
+            label2 = new Label();
+            txtSo = new MaskedTextBox();
+            txtKetQua = new MaskedTextBox();
+            btnDoc = new Button();
+            btnXoa = new Button();
+            btnThoat = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 87);
+            label1.Name = "label1";
+            label1.Size = new Size(264, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nhập vào số nguyên (tối đa 12 chữ số)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 252);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Kết quả";
+            label2.Click += label2_Click;
+            // 
+            // txtSo
+            // 
+            txtSo.Location = new Point(394, 80);
+            txtSo.Name = "txtSo";
+            txtSo.Size = new Size(125, 27);
+            txtSo.TabIndex = 2;
+            // 
+            // txtKetQua
+            // 
+            txtKetQua.Location = new Point(394, 245);
+            txtKetQua.Name = "txtKetQua";
+            txtKetQua.ReadOnly = true;
+            txtKetQua.Size = new Size(125, 27);
+            txtKetQua.TabIndex = 3;
+            // 
+            // btnDoc
+            // 
+            btnDoc.Location = new Point(617, 98);
+            btnDoc.Name = "btnDoc";
+            btnDoc.Size = new Size(94, 29);
+            btnDoc.TabIndex = 4;
+            btnDoc.Text = "Đọc";
+            btnDoc.UseVisualStyleBackColor = true;
+            btnDoc.Click += btnDoc_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(617, 157);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 5;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(617, 210);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(94, 29);
+            btnThoat.TabIndex = 6;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // Lab01_Bai04
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnThoat);
+            Controls.Add(btnXoa);
+            Controls.Add(btnDoc);
+            Controls.Add(txtKetQua);
+            Controls.Add(txtSo);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Lab01_Bai04";
+            Text = "Lab01_Bai04";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private MaskedTextBox txtSo;
+        private MaskedTextBox txtKetQua;
+        private Button btnDoc;
+        private Button btnXoa;
+        private Button btnThoat;
     }
 }
--- lab01/Lab01_Bai04.cs
+++ lab01/Lab01_Bai04.cs
@@ -14,5 +14,125 @@ namespace lab01
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDoc_Click(object sender, EventArgs e)
+        {
+            string input = txtSo.Text.Trim();
+
+            if (!long.TryParse(input, out long n) || n < 0 || input.Length > 12)
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên dương tối đa 12 chữ số!", "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (n == 0)
+            {
+                txtKetQua.Text = "Không";
+                return;
+            }
+
+            txtKetQua.Text = DocSoThanhChu(n);
+        }
+
+      
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            txtSo.Clear();
+            txtKetQua.Clear();
+            txtSo.Focus();
+        }
+
+      
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private string DocSoThanhChu(long number)
+        {
+            int ty = (int)((number / 1000000000) % 1000);
+            int trieu = (int)((number / 1000000) % 1000);
+            int ngan = (int)((number / 1000) % 1000);
+            int donVi = (int)(number % 1000);
+
+            string ketQua = "";
+            bool daCoLopTruoc = false;
+
+            if (ty > 0)
+            {
+                ketQua += Doc3So(ty, daCoLopTruoc) + " tỷ ";
+                daCoLopTruoc = true;
+            }
+
+            if (trieu > 0)
+            {
+                ketQua += Doc3So(trieu, daCoLopTruoc) + " triệu ";
+                daCoLopTruoc = true;
+            }
+
+            if (ngan > 0)
+            {
+                ketQua += Doc3So(ngan, daCoLopTruoc) + " ngàn ";
+                daCoLopTruoc = true;
+            }
+
+            if (donVi > 0)
+            {
+                ketQua += Doc3So(donVi, daCoLopTruoc);
+            }
+
+            ketQua = ketQua.Trim();
+
+            if (!string.IsNullOrEmpty(ketQua))
+            {
+                ketQua = char.ToUpper(ketQua[0]) + ketQua.Substring(1);
+            }
+
+            return ketQua;
+        }
+
+        private string Doc3So(int number, bool full)
+        {
+            string[] chuSo = { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+            int tram = number / 100;
+            int chuc = (number % 100) / 10;
+            int dv = number % 10;
+            string res = "";
+
+            if (tram > 0 || full)
+            {
+                res += chuSo[tram] + " trăm ";
+            }
+
+            if (chuc > 1)
+            {
+                res += chuSo[chuc] + " mươi ";
+                if (dv == 1) res += "mốt";
+                else if (dv == 5) res += "lăm";
+                else if (dv > 0) res += chuSo[dv];
+            }
+            else if (chuc == 1)
+            {
+                res += "mười ";
+                if (dv == 5) res += "lăm";
+                else if (dv > 0) res += chuSo[dv];
+            }
+            else
+            {
+                if (dv > 0)
+                {
+                    if (tram > 0 || full) res += "lẻ ";
+                    res += chuSo[dv];
+                }
+            }
+
+            return res.Trim();
+        }
+    } 
+} 
