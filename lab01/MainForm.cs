e48fc53854d98dabe0a073dc02d7e7b0c4984131
Anh <hoanganh12052007@gmail.com>
2026-09-25 17:22:12 +0700

    fix mainform.cs

--- lab01/MainForm.cs
+++ lab01/MainForm.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
 using System.Windows.Forms;
+
 
 namespace lab01
 {
