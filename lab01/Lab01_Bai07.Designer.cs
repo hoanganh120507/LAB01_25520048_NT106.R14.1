d7d71b7dbfe6f188721a40d974e48f56a9a6f9a5
Anh <hoanganh12052007@gmail.com>
2026-09-26 23:43:16 +0700

    updated bai 7

--- lab01/Lab01_Bai07.Designer.cs
+++ lab01/Lab01_Bai07.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Lab01_Bai07
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,96 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Lab01_Bai07";
+            lblNgaySinh = new Label();
+            dtpNgaySinh = new DateTimePicker();
+            btnXacDinh = new Button();
+            lblKetQua = new Label();
+            txtCungHoangDao = new TextBox();
+            btnThoat = new Button();
+            SuspendLayout();
+            // 
+            // lblNgaySinh
+            // 
+            lblNgaySinh.AutoSize = true;
+            lblNgaySinh.Location = new Point(40, 40);
+            lblNgaySinh.Name = "lblNgaySinh";
+            lblNgaySinh.Size = new Size(153, 20);
+            lblNgaySinh.TabIndex = 0;
+            lblNgaySinh.Text = "Chọn ngày tháng năm:";
+            // 
+            // dtpNgaySinh
+            // 
+            dtpNgaySinh.CustomFormat = "dd/MM/yyyy";
+            dtpNgaySinh.Format = DateTimePickerFormat.Custom;
+            dtpNgaySinh.Location = new Point(200, 35);
+            dtpNgaySinh.Name = "dtpNgaySinh";
+            dtpNgaySinh.Size = new Size(180, 27);
+            dtpNgaySinh.TabIndex = 1;
+            // 
+            // btnXacDinh
+            // 
+            btnXacDinh.Location = new Point(200, 80);
+            btnXacDinh.Name = "btnXacDinh";
+            btnXacDinh.Size = new Size(95, 32);
+            btnXacDinh.TabIndex = 2;
+            btnXacDinh.Text = "Xác định";
+            btnXacDinh.UseVisualStyleBackColor = true;
+            btnXacDinh.Click += btnXacDinh_Click;
+            // 
+            // lblKetQua
+            // 
+            lblKetQua.AutoSize = true;
+            lblKetQua.Location = new Point(40, 140);
+            lblKetQua.Name = "lblKetQua";
+            lblKetQua.Size = new Size(149, 20);
+            lblKetQua.TabIndex = 3;
+            lblKetQua.Text = "Cung hoàng đạo là:";
+            // 
+            // txtCungHoangDao
+            // 
+            txtCungHoangDao.Location = new Point(200, 137);
+            txtCungHoangDao.Name = "txtCungHoangDao";
+            txtCungHoangDao.ReadOnly = true;
+            txtCungHoangDao.Size = new Size(200, 27);
+            txtCungHoangDao.TabIndex = 4;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(305, 80);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(95, 32);
+            btnThoat.TabIndex = 5;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // Lab01_Bai07
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(450, 200);
+            Controls.Add(btnThoat);
+            Controls.Add(txtCungHoangDao);
+            Controls.Add(lblKetQua);
+            Controls.Add(btnXacDinh);
+            Controls.Add(dtpNgaySinh);
+            Controls.Add(lblNgaySinh);
+            Name = "Lab01_Bai07";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Bài 07 - Tra cứu Cung Hoàng Đạo";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblNgaySinh;
+        private DateTimePicker dtpNgaySinh;
+        private Button btnXacDinh;
+        private Label lblKetQua;
+        private TextBox txtCungHoangDao;
+        private Button btnThoat;
     }
 }
--- lab01/Lab01_Bai07.cs
+++ lab01/Lab01_Bai07.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 namespace lab01
@@ -13,6 +8,62 @@ namespace lab01
         public Lab01_Bai07()
         {
             InitializeComponent();
+        }
+
+        private void btnXacDinh_Click(object sender, EventArgs e)
+        {
+            int day = dtpNgaySinh.Value.Day;
+            int month = dtpNgaySinh.Value.Month;
+
+            string cungHoangDao = "";
+
+            // Xử lý logic switch-case theo từng tháng sinh
+            switch (month)
+            {
+                case 1:
+                    cungHoangDao = (day <= 20) ? "Ma Kết" : "Bảo Bình";
+                    break;
+                case 2:
+                    cungHoangDao = (day <= 19) ? "Bảo Bình" : "Song Ngư";
+                    break;
+                case 3:
+                    cungHoangDao = (day <= 20) ? "Song Ngư" : "Bạch Dương";
+                    break;
+                case 4:
+                    cungHoangDao = (day <= 20) ? "Bạch Dương" : "Kim Ngưu";
+                    break;
+                case 5:
+                    cungHoangDao = (day <= 21) ? "Kim Ngưu" : "Song Tử";
+                    break;
+                case 6:
+                    cungHoangDao = (day <= 21) ? "Song Tử" : "Cự Giải";
+                    break;
+                case 7:
+                    cungHoangDao = (day <= 22) ? "Cự Giải" : "Sư Tử";
+                    break;
+                case 8:
+                    cungHoangDao = (day <= 22) ? "Sư Tử" : "Xử Nữ";
+                    break;
+                case 9:
+                    cungHoangDao = (day <= 23) ? "Xử Nữ" : "Thiên Bình";
+                    break;
+                case 10:
+                    cungHoangDao = (day <= 23) ? "Thiên Bình" : "Thần Nông";
+                    break;
+                case 11:
+                    cungHoangDao = (day <= 22) ? "Thần Nông" : "Nhân Mã";
+                    break;
+                case 12:
+                    cungHoangDao = (day <= 21) ? "Nhân Mã" : "Ma Kết";
+                    break;
+            }
+
+            txtCungHoangDao.Text = cungHoangDao;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
